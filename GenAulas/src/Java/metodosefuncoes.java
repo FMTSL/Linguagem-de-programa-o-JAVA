3b7c512d249d72b668a648fdf17b9293b6a1c4b1
Felipe Matos <felipe_matos@hotmail.com.br>
2023-03-07 10:15:27 -0300

    Metodos e Funções

---
+++ GenAulas/src/Java/metodosefuncoes.java
@@ -0,0 +1,11 @@
+package Java;
+
+public class metodosefuncoes {
+	static void myMethod (String fname) {
+		System.out.println(fname + "Brenda");
+	}
+	
+	public static void main (String[] args) {
+		myMethod("Felipe");
+	}
+}
